697c17d7130b7997a6da4dc450e490fd4e2c353b
kenkirito <lakshya.mishra56@gmail.com>
2021-07-02 22:52:13 +0530

    init

---
+++ lib/Pages/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text("loginPgae"),
+      ),
+    );
+  }
+}
