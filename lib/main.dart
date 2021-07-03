2eb44d8a2830f45d93a713de495f967142d49fd3
kenkirito <lakshya.mishra56@gmail.com>
2021-07-03 20:57:03 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => Homepage(),
