d5872065dac607462ac75ff7c4dd59ef057ab367
kenkirito <lakshya.mishra56@gmail.com>
2021-07-04 02:27:06 +0530

    init

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -4,16 +4,10 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "LoginPage",
-          style: TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-          textScaleFactor: 6.0,
-        ),
-      ),
-    );
+        child: Column(
+      children: [
+        Image.asset("assests/images/Login_home.png"),
+      ],
+    ));
   }
 }
