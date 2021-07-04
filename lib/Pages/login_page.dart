c9bffcd18aa4de964c6ab875811b45d25c8411f7
kenkirito <lakshya.mishra56@gmail.com>
2021-07-04 22:52:18 +0530

    init

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -23,14 +23,26 @@ class LoginPage extends StatelessWidget {
           SizedBox(
             height: 20.0,
           ),
-          TextFormField(
-            decoration: InputDecoration(
-                hintText: "Enter username", labelText: "Username"),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                hintText: "Enter password", labelText: "Password"),
-          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password", labelText: "Password"),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+              ],
+            ),
+          )
         ]));
   }
 }
