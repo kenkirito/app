2e2b7e6fe7a7750471bbc69d4ddd19b9638117b7
kenkirito <lakshya.mishra56@gmail.com>
2021-07-04 21:58:57 +0530

    init

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -5,13 +5,32 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assests/images/Login_home.png",
-              fit: BoxFit.cover,
+        child: Column(children: [
+          Image.asset(
+            "assests/images/Login_home.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "welcome",
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
             ),
-          ],
-        ));
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: "Enter username", labelText: "Username"),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: "Enter password", labelText: "Password"),
+          ),
+        ]));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/Pages/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'Pages/home_page.dart';
 
 void main() {
@@ -14,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.laBelleAurore().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
