62e64e8ef5281545c810cea9a5b459efc75b048f
kenkirito <lakshya.mishra56@gmail.com>
2021-07-15 03:10:57 +0530

    👌 IMPROVE: use of json data

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_app/Pages/models/category.dart';
 import 'package:flutter_app/widge/drawer.dart';
 import 'package:flutter_app/widge/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final double days = 30;
+
   final String name = "coderpur";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assests/files/catalog.json");
+    var decordedData = jsonDecode(catalogJson);
+    var productsData = decordedData["products"];
+  }
 
   @override
   Widget build(BuildContext context) {
