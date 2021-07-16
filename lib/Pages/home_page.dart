a4c88045eed9f6998589e6bf5c0cf186188efb23
kenkirito <lakshya.mishra56@gmail.com>
2021-07-16 06:22:58 +0530

    📦 NEW: Velocity x

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:flutter_app/Pages/models/catalog.dart';
 import 'package:flutter_app/widge/drawer.dart';
 import 'package:flutter_app/widge/item_widget.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -31,50 +32,10 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog app"),
-      ),
-      body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: (CatalogModel.items.isNotEmpty)
-              ? GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16,
-                  ),
-                  itemBuilder: (context, index) {
-                    final item = CatalogModel.items[index];
-                    return Card(
-                        clipBehavior: Clip.antiAlias,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: GridTile(
-                          header: Container(
-                            child: Text(
-                              item.name,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(color: Colors.deepPurple),
-                          ),
-                          child: Image.network(item.image),
-                          footer: Container(
-                            child: Text(
-                              item.price.toString(),
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(color: Colors.black),
-                          ),
-                        ));
-                  },
-                  itemCount: CatalogModel.items.length,
-                )
-              : Center(
-                  child: CircularProgressIndicator(),
-                )),
-      drawer: MyDrawer(),
-    );
+        body: Column(
+      children: [
+        "Catalog App".text.make(),
+      ],
+    ));
   }
 }
--- lib/widge/theme.dart
+++ lib/widge/theme.dart
@@ -16,4 +16,8 @@ class MyTheme {
 
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
+
+  //colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
