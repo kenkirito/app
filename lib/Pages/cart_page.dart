71cf08c5e567a6efa7d435990bc78cf01bcf6613
kenkirito <lakshya.mishra56@gmail.com>
2021-07-26 22:56:03 +0530

    📦 NEW

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -11,6 +11,26 @@ class Cartpage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
       ),
+      body: Column(
+        children: [_CartList().p32().expand()],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  _CartListState createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+      ),
     );
   }
 }
